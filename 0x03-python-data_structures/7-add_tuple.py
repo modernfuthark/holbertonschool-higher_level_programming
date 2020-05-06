e773b19dc3f6658ea965d29e1278ea46cc05181b
modernfuthark <timothys1738@gmail.com>
2020-05-06 10:13:35 -0400

    Created file and inserted into directory

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuble_b=()):
-    if 
+def add_tuple(tuple_a=(), tuple_b=()):
+    result = [0, 0]
+    if len(tuple_a) >= 1:
+        result[0] = result[0] + tuple_a[0]
+    if len(tuple_a) >= 2:
+        result[1] = result[1] + tuple_a[1]
+
+    if len(tuple_b) >= 1:
+        result[0] = result[0] + tuple_b[0]
+    if len(tuple_b) >= 2:
+        result[1] = result[1] + tuple_b[1]
+
+    return (result[0], result[1])
