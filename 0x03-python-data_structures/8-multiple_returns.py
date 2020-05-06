325ff2e0e8d00cc5cc32d1aef4114a2ccf2ed027
modernfuthark <timothys1738@gmail.com>
2020-05-05 20:23:50 -0400

    Flopped the values of tuple if len = 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        data = (None, 0)
+        data = (0, None)
     else:
         data = (len(sentence), sentence[0])
     return (data)
