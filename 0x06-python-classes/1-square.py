d2aaaef9523032eb099450b2b64939bc2c79477f
modernfuthark <timothys1738@gmail.com>
2020-05-19 15:38:48 -0400

    Added file to directory

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 1-square: File for the Square class"""
+
+
+class Square:
+    """Square: Square class"""
+    def __init__(self, size=0):
+        """__init__: Init method"""
+        self.__size = size
