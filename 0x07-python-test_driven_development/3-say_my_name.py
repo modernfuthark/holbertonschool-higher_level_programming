a7d75454b8905f29ecbcbfcae1698adacc0920ab
modernfuthark <timothys1738@gmail.com>
2020-05-21 18:25:57 -0400

    Added files to directory

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 3-say_my_name: Print someone's name """
+
+
+def say_my_name(first_name, last_name=""):
+    """ say_my_name: Print the name from input """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
