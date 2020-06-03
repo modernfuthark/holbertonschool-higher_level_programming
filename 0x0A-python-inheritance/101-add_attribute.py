5bceb8e2a5bcc3ece7e587b9a797b0fede6a7e16
modernfuthark <timothys1738@gmail.com>
2020-06-02 21:54:14 -0400

    [+] New method of testing using an empty class

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ 101-add_attribute: Add an attribute to a class """
 
+class Empty:
+    """ Empty class """
+    pass
 
 def add_attribute(obj, name, val):
     """ add_attribute: Adds an attribute to obj """
-    if isinstance(obj, type) is False:
+    tester = Empty()
+    if type(obj) is not type(tester):
         raise TypeError("can't add new attribute")
     setattr(obj, name, val)
