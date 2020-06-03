14c9be8bd7af9a9b9b4176b636c32d414f0a03ac
modernfuthark <timothys1738@gmail.com>
2020-06-02 22:28:12 -0400

    [^] Updated file

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ 101-add_attribute: Add an attribute to a class """
 
-class Empty:
-    """ Empty class """
-    pass
 
 def add_attribute(obj, name, val):
     """ add_attribute: Adds an attribute to obj """
-    tester = Empty()
-    if type(obj) is not type(tester):
+    if isinstance(obj, type) is False:
         raise TypeError("can't add new attribute")
-    setattr(obj, name, val)
+        return
+    if hasattr(obj, '__init__') is True:
+        setattr(obj, name, val)
+    else:
+        raise TypeError("can't add new attribute")
