0b6c25a9603f1d195001a9383a5169816c040294
modernfuthark <timothys1738@gmail.com>
2020-06-02 21:35:59 -0400

    [N] Added file for adv task 12 to directory

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,3 +4,10 @@
 
 class MyInt(int):
     """ MyInt: A rebellious int """
+    def __eq__(self, other):
+        """ __eq__: Equals """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """ __ne__: Not equals """
+        return super().__eq__(other)
