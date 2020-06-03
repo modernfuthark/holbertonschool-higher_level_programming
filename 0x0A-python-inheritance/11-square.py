12501f5ea484535497671629a106f0ce2d82645a
modernfuthark <timothys1738@gmail.com>
2020-06-02 21:20:51 -0400

    [N] Added file for task 11 to directory

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 11-square: File for the Square class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square: A square class """
+
+    def __init__(self, size):
+        """ __init__: Initial method """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ area: Return the area """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ __str__: String implementation """
+        return "[Square] {}/{}".format(self.__size, self.__size)
