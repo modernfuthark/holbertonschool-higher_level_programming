72ab4bf1a84628c93e2d02bee46005415af57a92
modernfuthark <timothys1738@gmail.com>
2020-06-02 20:27:35 -0400

    [N] Added file for task 6 to directory

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 6-base_geometry: Creates an empty class BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseGeometry: Empty class """
+
+    def area(self):
+        """ area: Does nothing at the moment """
+        raise Exception("area() is not implemented")
