b1b0b8608b1a44b8cb9cc49e4fbf484db9d66ffc
modernfuthark <timothys1738@gmail.com>
2020-06-03 20:30:19 -0400

    [N] Added file for task 9 to directory

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 9-add_item: Add all arguments to a list """
+import json
+import sys
+jsonsave = __import__('7-save_to_json_file').save_to_json_file
+jsonload = __import__('8-load_from_json_file').load_from_json_file
+
+def main():
+    try:
+        arglist = jsonload("add_item.json")
+    except:
+        arglist = []
+    for i in range(1, len(sys.argv)):
+        arglist.append(sys.argv[i])
+    jsonsave(arglist, "add_item.json")
+
+if __name__ == "__main__":
+    main()
