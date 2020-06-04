4c3b52ee8037a440a65400cee34785b2192cbce7
modernfuthark <timothys1738@gmail.com>
2020-06-03 21:19:22 -0400

    [N] Added unfinished file for task 14 to directory

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" 14-pascal_triangle: Create a Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """ pascal_triangle: Create a Pascal's Triangle of size n """
+    if n <= 0:
+        return []
