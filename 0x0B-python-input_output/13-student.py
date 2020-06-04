e56706f2d67e97a30b027b15bb18c1fc7b3f4366
modernfuthark <timothys1738@gmail.com>
2020-06-03 21:09:37 -0400

    [N] Added file for task 13 to directory

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" 13-student: File for the Student class """
+
+
+class Student:
+    """ Student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ __init__: Initital method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to_json: Returns the dictionary of self """
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+
+        new = {}
+        for element in attrs:
+            if type(element) is not str:
+                return self.__dict__
+            try:
+                new[element] = getattr(self, element)
+            except:
+                pass
+        return new
+
+    def reload_from_json(self, json):
+        """ reload_from_json: Replace all attrs of Student """
+        for key in json:
+            setattr(self, key, json[i])
