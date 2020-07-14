2931c6616d75481addba8fe1073b2adcaee3918f
modernfuthark <timothys1738@gmail.com>
2020-07-14 14:45:22 -0400

    [^] Accidentally wrote IF NOT NULL instead of IF NOT EXISTS

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- 4-never_empty
 -- Creates a table 'id_not_null' with id's default value being '1'
-CREATE TABLE IF NOT NULL id_not_null (id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
