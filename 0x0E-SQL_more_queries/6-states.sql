c8071c23d4375d1053989c351cd1317717a05689
modernfuthark <timothys1738@gmail.com>
2020-07-14 15:13:19 -0400

    [^] Removed extra IS

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,4 @@
 -- Creates database 'hbtn_0d_usa' and table 'states', 'id' of states if unique and a primary key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT IS NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
