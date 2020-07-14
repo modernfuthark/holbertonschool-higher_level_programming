41afbe0742cd4449d3fd806b443035bbb9fed435
modernfuthark <timothys1738@gmail.com>
2020-07-14 16:18:03 -0400

    [^] Fixed capitalization of FROM

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 -- 8-cities_of_california_subquery
 -- lists all cities of California that can be found in the database
-SELECT id, name from cities WHERE state_id = 
-(SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
