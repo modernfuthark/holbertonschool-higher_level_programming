2a9a7dc95c54908f373df15fe9741bc2eb18fa39
modernfuthark <timothys1738@gmail.com>
2020-07-14 17:56:54 -0400

    [N] Added file for task 11 to directory

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- 11-genre_id_all_shows
+-- Display all the genre IDs of all shows, if no genre ID is present, display NULL
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
