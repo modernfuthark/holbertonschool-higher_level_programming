0acf0851e8aa618d165433f14cb9ec04f862a42c
modernfuthark <timothys1738@gmail.com>
2020-07-14 16:16:47 -0400

    [^] maybe a linebreak will help ??

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- 8-cities_of_california_subquery
 -- lists all cities of California that can be found in the database
-SELECT id, name from cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
+SELECT id, name from cities WHERE state_id = 
+(SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
