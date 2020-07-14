9a9eeb6da9866cdb4644fa00bfc0df20ac458639
modernfuthark <timothys1738@gmail.com>
2020-07-14 18:42:57 -0400

    [N] Added file for task 15 to directory

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,3 @@
+-- 15-comedy_only
+-- Displays only shows that have the comedy genre
+SELECT tv_shows.title FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC;
