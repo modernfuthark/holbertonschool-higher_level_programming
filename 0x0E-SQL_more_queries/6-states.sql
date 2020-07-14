16b15a9d0a7657a2f7753207db8a5632bc215526
modernfuthark <timothys1738@gmail.com>
2020-07-14 15:08:31 -0400

    [N] Added file for task 6 to directory

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- 6-states
+-- Creates database 'hbtn_0d_usa' and table 'states', 'id' of states if unique and a primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT IS NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
