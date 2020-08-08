4a21c51eff0949e6fe18567ea50a95a249161ccb
modernfuthark <timothys1738@gmail.com>
2020-08-08 19:28:54 -0400

    [N] Added file to directory

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Selects the states from an SQL database that match a user input """
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """ Main function """
+    sql_User = argv[1]
+    sql_Password = argv[2]
+    sql_dbName = argv[3]
+
+    sql_database = MySQLdb.connect(
+        host="localhost",
+        user=sql_User,
+        passwd=sql_Password,
+        db=sql_dbName,
+        port=3306
+    )
+
+    cur = sql_database.cursor()
+
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+
+    # print(cur.fetchall())
+    for i in cur.fetchall():
+        if i[1] == argv[4]:
+            print(i)
+
+if __name__ == "__main__":
+    main()
