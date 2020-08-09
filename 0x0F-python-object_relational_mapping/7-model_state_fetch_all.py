bf20507181524297ee4bbba560675326d941b021
modernfuthark <timothys1738@gmail.com>
2020-08-08 21:15:54 -0400

    [N] Added file to directory for task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Using SQLAlchemy, get all states from a database """
+
+
+def Main():
+    """ Main function """
+
+    from sys import argv
+    from model_state import Base, State
+    # These must be import-protected
+    # (don't remember if this was true or not, better safe than sorry)
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm.session import Session, sessionmaker
+
+    sql_username = str(argv[1])
+    sql_passwd = str(argv[2])
+    sql_dbname = str(argv[3])
+
+    sqla_engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(sql_username, sql_passwd, sql_dbname)
+        )
+
+    Session = sessionmaker(bind=sqla_engine)
+    sqla_session = Session()
+
+    for i in sqla_session.query(State).order_by(State.id):
+        print('{}: {}'.format(i.id, i.name))
+
+if __name__ == "__main__":
+    Main()
