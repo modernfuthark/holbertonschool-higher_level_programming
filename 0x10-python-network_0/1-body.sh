18bb84de41cc0ebd2bada6af8c90a45996e0a8cc
modernfuthark <timothys1738@gmail.com>
2020-08-18 18:01:25 -0400

    [^] Updated quotation marks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Sends a GET request to a URL, if the response is of status code 200, display the content of the body.
-if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d\" \" -f2)" -eq "200" ];
-then
+if [ "$(curl -sLI "$1" -X GET | grep '200 OK' | cut -d' ' -f2)" -eq '200' ]; then
 	curl -sL "$1";
 fi
