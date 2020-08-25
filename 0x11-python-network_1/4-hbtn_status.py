e66bb61e7e43e1c11834c46b78845e6469f9cd20
modernfuthark <timothys1738@gmail.com>
2020-08-24 21:28:42 -0400

    [^9] fixed spelling error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ import requests
 
 if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status")
-    print("Body responce:")
+    print("Body response:")
     print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.text))
