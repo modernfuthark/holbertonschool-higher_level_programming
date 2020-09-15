0e2c7f2bae48d9b45ad71eff861cffadc10786bf
modernfuthark <timothys1738@gmail.com>
2020-09-15 19:36:07 -0400

    [N] Added file for task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+
+while (strings[i]) {
+  console.log(strings[i]);
+  i++;
+}
+
+/* For loop, just for learnings sake */
+/*
+for (let i = 0; strings[i]; i++) {
+  console.log(strings[i])
+}
+*/
