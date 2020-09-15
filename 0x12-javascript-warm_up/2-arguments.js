c965c973194abd8076546277afee28e4b5c486d4
modernfuthark <timothys1738@gmail.com>
2020-09-15 19:12:44 -0400

    [N] Added file for task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* console.log(process.argv.length); */
+
+if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No arguments');
+}
