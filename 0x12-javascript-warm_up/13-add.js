b240d882478e5dfd04fcb050f30f773323487372
modernfuthark <timothys1738@gmail.com>
2020-09-15 20:55:52 -0400

    [^] Made the function wayyy simpler

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 
 exports.add = function (a, b) {
-  const n1 = parseInt(a);
-  const n2 = parseInt(b);
-  if (n1 && n2) {
-    return n1 + n2;
-  } else {
-    return NaN;
-  }
+  return a + b;
 };
