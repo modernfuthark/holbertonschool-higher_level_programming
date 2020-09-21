09194f2515cb6eae4f25099f18fb6d471f622046
modernfuthark <timothys1738@gmail.com>
2020-09-21 18:54:51 -0400

    [^] dunno if checker dislikes us using require()

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,30 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const hold = this.width;
+    this.width = this.height;
+    this.height = hold;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
 
 class Square extends Rectangle {
   constructor (size) {
