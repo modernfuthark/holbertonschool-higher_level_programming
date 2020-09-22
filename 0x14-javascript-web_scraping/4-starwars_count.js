165f93302c79eef7b2d1d7e03e2c5e66a2a40c83
modernfuthark <timothys1738@gmail.com>
2020-09-22 19:11:18 -0400

    [^] Throws err instead of normal console.log

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,13 @@ const url = process.argv[2];
 const char = 'https://swapi-api.hbtn.io/api/people/18/';
 
 req(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    let count = 0;
-    for (const film of JSON.parse(body).results) {
-      if (film.characters.includes(char)) {
-        count++;
-      }
+  if (err) throw err;
+
+  let count = 0;
+  for (const film of JSON.parse(body).results) {
+    if (film.characters.includes(char)) {
+      count++;
     }
-    console.log(count);
   }
+  console.log(count);
 });
