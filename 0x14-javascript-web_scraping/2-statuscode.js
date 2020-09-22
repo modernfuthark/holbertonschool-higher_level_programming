799ba6c2a69f6f5736ed86a26db10e6f1a0af737
modernfuthark <timothys1738@gmail.com>
2020-09-22 18:46:32 -0400

    [N] Added file for task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const req = require('request');
+
+req(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code : ${res.statusCode}`);
+  }
+});
