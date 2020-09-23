df263ed284162f6ddff323345846e1f49127aa1c
modernfuthark <timothys1738@gmail.com>
2020-09-23 13:43:43 -0400

    [+] Added a 2nd loop

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,15 +2,17 @@
 
 const req = require('request');
 const url = process.argv[2];
-const char = 'https://swapi-api.hbtn.io/api/people/18/';
+/* const char = 'https://swapi-api.hbtn.io/api/people/18/'; */
 let count = 0;
 
 req(url, (err, res, body) => {
   if (err) throw err;
 
   for (const film of JSON.parse(body).results) {
-    if (film.characters.includes(char)) {
-      count++;
+    for (const char of film.characters) {
+      if (char.includes('/people/18')) {
+        count++;
+      }
     }
   }
   console.log(count);
